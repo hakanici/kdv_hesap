5a40de0f0392a6270c8d2317d5d8e14ac171097c
hakanici <hakanici123@gmail.com>
2024-05-13 18:24:06 +0300

    week 2 harmonik odev

--- src/week2/Harmonik.java
+++ src/week2/Harmonik.java
@@ -3,7 +3,19 @@ package week2;
 public class Harmonik {
     public static void main(String[] args) {
 
-        
+        int[] arr = {10, 2, 3, 4, 5};
+        double toplam = 0.0;
+        double harm;
+
+        for (int j : arr) {
+
+
+            toplam += (double) 1 / j;
+
+        }
+        harm = arr.length / toplam;
+        System.out.println(harm);
+
 
     }
 }
