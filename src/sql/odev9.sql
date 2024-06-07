3fd94e3ece343b6f889597df9f1181057efa23e3
hakanici <hakanici123@gmail.com>
2024-06-07 15:58:19 +0300

    week 6 sql dev

---
+++ src/sql/odev9.sql
@@ -0,0 +1,11 @@
+--city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city,country from city
+JOIN country ON country.country_id=city.country_id;
+
+--customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT payment_id,first_name,last_name from customer
+JOIN payment ON payment.customer_id=customer.customer_id;
+
+--customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT rental_id,first_name,last_name from customer
+JOIN rental ON rental.customer_id=customer.customer_id;
